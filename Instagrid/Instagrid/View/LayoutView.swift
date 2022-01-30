ce4f1349b15c76246044db1dbfd570cba8e348e9
GiovanniGBS <giovannigbs@icloud.com>
2022-01-30 13:28:27 -0400

    Design change :  Orientation Portrait / Paysage

---
+++ Instagrid/Instagrid/View/LayoutView.swift
@@ -0,0 +1,15 @@
+//
+//  LayoutView.swift
+//  Instagrid
+//
+//  Created by Giovanni Gabriel on 24/01/2022.
+//
+
+import UIKit
+
+class LayoutView: UIView {
+
+    var style: UIImage
+    var icon : UIImageView?
+
+}
