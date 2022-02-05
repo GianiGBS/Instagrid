2b1862d45e3eefdd218278e597fa0534f7652329
GiovanniGBS <giovannigbs@icloud.com>
2022-02-05 19:32:03 -0400

    Design : Buttons Action

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Top HStack
+    
+    
+    // Layout View
+    @IBOutlet weak var layoutView: LayoutView!
+    
+    // Bottom HStack
+    @IBOutlet weak var layout1Button: UIButton!
+    @IBOutlet weak var layout2Button: UIButton!
+    @IBOutlet weak var layout3Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func didTapLayout1Button() {
+        //changeLayout()
+        layoutView.style = .layout1
+    }
+    @IBAction func didTapLayout2Button() {
+        //changeLayout()
+        layoutView.style = .layout2
+    }
+    @IBAction func didTapLayout3Button() {
+        //changeLayout()
+        layoutView.style = .layout3
+    }
+    
+    private func changeLayout() {
+        // Modifier image du bouton
+        // Modifier le style depuis LayoutView
+    }
+    
 }
-
--- Instagrid/Instagrid/View/LayoutView.swift
+++ Instagrid/Instagrid/View/LayoutView.swift
@@ -8,8 +8,42 @@
 import UIKit
 
 class LayoutView: UIView {
+    
+// Top stack View
+    @IBOutlet var firstTopImage : UIImageView?
+    @IBOutlet var secondTopImage : UIImageView?
+ // Bottom stack View
+    @IBOutlet var firstBottomImage : UIImageView?
+    @IBOutlet var secondBottomImage : UIImageView?
 
-    var style: UIImage
-    var icon : UIImageView?
-
+    enum Style {
+        case layout1, layout2, layout3
+    }
+    
+    var style : Style = .layout3 {
+        didSet {
+            setStyle(style)
+        }
+    }
+    
+    private func setStyle(_ style: Style) {
+        switch style {
+        case .layout1:
+            firstTopImage?.image = UIImage()
+            secondTopImage?.isHidden = true
+            firstBottomImage?.image = UIImage()
+            secondBottomImage?.image = UIImage()
+        case .layout2:
+            firstTopImage?.image = UIImage()
+            secondTopImage?.image = UIImage()
+            firstBottomImage?.image = UIImage()
+            secondBottomImage?.isHidden = true
+        case .layout3:
+            firstTopImage?.image = UIImage()
+            secondTopImage?.image = UIImage()
+            firstBottomImage?.image = UIImage()
+            secondBottomImage?.image = UIImage()
+        }
+        
+    }
 }
